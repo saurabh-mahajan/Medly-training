c4b658acdb076d26908901d59b788158a1bf5a63
Saurabh Mahajan <saurabhmahajan@Apples-MacBook-Pro-5.local>
2022-05-23 14:20:00 +0530

    update the test cases and updated the logic accordingly for the failed testcases

--- anagram-checker/src/AnagramChecker.kt
+++ anagram-checker/src/AnagramChecker.kt
@@ -2,9 +2,11 @@ class AnagramChecker {
     fun match(word1: String, word2: String): Boolean {
         val counter = IntArray(26) { 0 }
         if (word1.length != word2.length) return false
-        (word1.indices).forEach { index ->
-            ++counter[word1[index] - 'a']
-            --counter[word2[index] - 'a']
+        var res1 = word1.lowercase()
+        var res2 = word2.lowercase()
+        (res1.indices).forEach { index ->
+            ++counter[res1[index] - 'a']
+            --counter[res2[index] - 'a']
         }
         return !counter.any { it != 0 }
     }
--- anagram-checker/test/AnagramCheckerTest.kt
+++ anagram-checker/test/AnagramCheckerTest.kt
@@ -1,10 +1,24 @@
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 
 class AnagramCheckerTest : StringSpec() {
     init {
-        "is 'a' an anagram of 'a'" {
+        "is 'b' an anagram of 'bac', check for not equal lengths"{
+            AnagramChecker().match("b","bac").shouldBeFalse()
+        }
+        "is 'a' an anagram of 'a', check for equal length string & both string in small-case" {
             AnagramChecker().match("a", "a").shouldBeTrue()
         }
+        "is 'DoG' an anagram of 'dog', check for uppercase and lowercase equality"{
+            AnagramChecker().match("DoG","dog").shouldBeTrue()
+        }
+        "is 'A' an anagram of 'A', check for equal length string & both string in capital-case"{
+            AnagramChecker().match("A","A").shouldBeTrue()
+        }
+        "is 'A' an anagram of 'aA', check for the same character occurrence but when count is not equal"{
+            AnagramChecker().match("A","aA").shouldBeFalse()
+        }
+
     }
 }
